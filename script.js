4fb430d5b4245ab032b54389c9a22beb683c09fe
Joilton Alvino <joiltonEstTrab@outlook.com>
2023-09-27 17:38:02 -0300

    :tada: inital commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  html.classList.toggle("light")
+  // if (html.classList.contains("light")) {
+  //   html.classList.remove("light")
+  // } else {
+  //   html.classList.add("light")
+  // }
+
+  //pegar a tag image
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", "Essa foto de Joilton Alvino com ceu de fundo")
+  } else {
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("alt", "Essa foto de Joilton Alvino na faculdade")
+  }
+}
